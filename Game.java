e5d3a3c739c5f009a4af1d8508200a3fd52016f1
Alex Beyderman <beyderman.alex@gmail.com>
2021-02-22 21:00:24 -0500

    added layout

--- Game.java
+++ Game.java
@@ -1,6 +1,9 @@
 
 import javax.swing.SwingUtilities;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.*;
 
 public class Game {
   public static void main(String[] args) {
@@ -14,9 +17,31 @@ public class Game {
   private static void createAndShowGUI() {
     JFrame f = new JFrame("Swing Paint Demo");
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.add(new GamePanel());
+    
+    f.setLayout(new GridBagLayout());
+
+    GridBagConstraints c = new GridBagConstraints();
+    c.fill = GridBagConstraints.HORIZONTAL;
+
+    c.gridx = 0;
+    c.gridy = 0;
+    f.add(new JTextField("User name"), c);
+
+    c.gridx = 1;
+    c.gridy = 0;
+    f.add(new JTextField("User score"), c);
+
+    c.gridx = 0;
+    c.gridy = 1;
+    c.gridwidth = 2;
+    f.add(new GamePanel(), c);
+
+    c.gridx = 0;
+    c.gridy = 2;
+    f.add(new JButton("Start Game"), c);
+
     f.pack();
+    f.setResizable(false);
     f.setVisible(true);
   }
 }
-
